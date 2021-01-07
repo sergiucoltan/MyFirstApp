fe8fd18c53eb80a9f41902332dfe6c9e59415b72
Sergiu Coltan <madsergoo@yahoo.com>
2021-01-07 21:56:32 +0200

    Changed Route config, added testcontroller, added error message and testview

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MyFirstApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Test",
+                url: "test/{action}",
+                defaults: new { controller = "Test", action = "PrintMessage"}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Controllers/TestController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TestView()
+        {
+            //return a 'testview.cshtml' - view maps to the action method name
+                return View();
+        }
+
+        public string Welcome(string Name, int numTimes = 1)
+        {
+            //return a 'testview.cshtml' - view maps to the action method name
+            return HttpUtility.HtmlEncode("Hello, " + Name + " Number of Times is " + numTimes);
+        }
+
+        public string Welcome2(string Name, int ID = 1)
+        {
+            //return a 'testview.cshtml' - view maps to the action method name
+            return HttpUtility.HtmlEncode("Hello, " + Name + " ID is " + ID);
+        }
+
+        public ActionResult ErrorMessage()
+        {
+            //return a 'testview.cshtml' - view maps to the action method name
+            return View();
+        }
+        public string PrintMessage()
+        {
+            return "<h1>Welcome</h1><p>This is the first custom page of your application";
+        }
+
+        public string Play()
+        {
+            return "<h1>Let's Play</h1><p>This is the first custom page of your application";
+        }
+    }
+}
